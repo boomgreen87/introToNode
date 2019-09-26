28c4e589f04abfb4fddabe75853bb44566cdb461
boomgreen87 <boomgreen87@yahoo.com>
2019-09-26 13:41:13 -0400

    Changed Port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1'; // This is localhost
-const port = 3000; // This is a node convention
+const port = process.env.PORT || 3000; // This is a node convention
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
